a73ded4dd3043d5e3873ff2ac838d76076680eba
charankumar7724 <108234180+charankumar7724@users.noreply.github.com>
2022-11-25 11:57:20 +0530

    Added controller and repo

---
+++ ATD_Multi-Insurance-Services/src/main/java/com/insurance/ATD/ChildInsurance/ChildController.java
@@ -0,0 +1,5 @@
+package com.insurance.ATD.ChildInsurance;
+
+public class ChildController {
+
+}
---
+++ ATD_Multi-Insurance-Services/src/main/java/com/insurance/ATD/ChildInsurance/ChildRepo.java
@@ -0,0 +1,7 @@
+package com.insurance.ATD.ChildInsurance;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ChildRepo extends JpaRepository<ChildBean, Integer> {
+
+}
